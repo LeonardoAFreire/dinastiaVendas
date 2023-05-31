6780c0db15fe1b07bf5ae380aa2d5f3d3861bfe6
Leonardo Freire <leoaguiarfreire@gmail.com>
2023-05-31 18:20:16 -0300

    modal ajustado mobile

---
+++ js/module/swiper.js
@@ -0,0 +1,32 @@
+export default function initSwiper() {
+  let swiper = new Swiper('.mySwiper', {
+    centerSlide: true,
+    grabCursor: true,
+    autoplay: {
+      delay: 0,
+    },
+    speed: 3000,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      // when window width is >= 640px
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
+}
