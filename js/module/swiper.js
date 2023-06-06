ba2bb75194aef44774b53d79d3911aff76f413f6
Leonardo Freire <leoaguiarfreire@gmail.com>
2023-06-06 19:38:08 -0300

    modal e parceiros

--- js/module/swiper.js
+++ js/module/swiper.js
@@ -30,4 +30,36 @@ export default function initSwiper() {
       },
     },
   });
+
+  let swiperParceiros = new Swiper('.swiperParceiros', {
+    centerSlide: true,
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+    speed: 500,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      // when window width is >= 640px
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
 }
